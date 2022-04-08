2d529eca89ed7df00b7fa0798b1a50b4bcb1aaae
vaibhavthapliyal <Thapliyal-V@ulster.ac.uk>
2022-04-08 11:46:58 +0100

    bug fixes in Search page

--- Pages/Students/Search.cshtml.cs
+++ Pages/Students/Search.cshtml.cs
@@ -4,29 +4,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Globalization;
+using System.Threading;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesAssignment1.Pages.Students
 {
     public class SearchModel : PageModel
-    {
-        private readonly  DetailsModel _context;
-        public IList<DetailsModel> Student
+    { 
+
+        protected void Page_Load(object sender, EventArgs e)
         {
-            get; set;
+            TextBoxSearch.Focus();
+            string selectCommand = "SELECT Details.Title AS Title, Details.LastName AS Surname, Details.FirstName  AS Name, Details.Email, Details.Phone " +
+                                    "FROM Details " +
+                                    "ORDER BY Details.LastName";
+
+            AccessDataSourceDetails.SelectCommand = selectCommand;
+
+            GridViewDetails.DataSource = AccessDataSourceDetails;
+            GridViewDetails.DataBind();
         }
-        [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
-        
- public  SearchModel (DetailsModel context)
+
+        /// <summary>
+        /// Search the text in the TextBoxSearch into the database fields and update the GridView contents
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void ButtonSearch_Click(object sender, EventArgs e)
         {
-            _context = context;
-        }
-        public async Task OnGetAsync()
-        {
-        }
-        public void OnGet()
-        {
+            if (TextBoxSearch.Text != "")
+            {
+                string search = TextBoxSearch.Text;
+                string selectCommand = "SELECT Details.Title AS Title, Details.LastName AS Surname, Details.FirstName  AS Name, Details.Email, Details.Phone " +
+                                       "FROM Details " +
+                                        "WHERE Details.LastName LIKE'%" + search + "%' OR " +
+                                        "Details.FirstName LIKE'%" + search + "%' OR " +
+                                        "Details.City LIKE'%" + search + "%' " +
+                                        "ORDER BY Details.LastName";
+
+                AccessDataSourceDetails.SelectCommand = selectCommand;
+
+                GridViewEDetails.DataSource = AccessDataSourceDetails;
+                GridViewDetails.DataBind();
+                TextBoxSearch.Focus();
+            }
         }
     }
+}
 }
